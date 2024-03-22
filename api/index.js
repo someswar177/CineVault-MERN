e4ea42997c2ba799d98e79e25f9a305f2a384125
someswar177 <someswarasai.p21@iiits.in>
2024-03-23 02:24:13 +0530

    added authorization jwt token

--- api/index.js
+++ api/index.js
@@ -5,14 +5,21 @@ const mongoose = require('mongoose');
 const dbConnect = require('./dbConnect');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const cors = require('cors')
 
 const User = require('./models/userModel');
 const Bookmark = require('./models/bookmarkModel');
+const auth = require('./auth')
 
 dbConnect();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(cors())
 
 app.listen(3000, () => {
     console.log("server is listening at http://localhost:3000");
+})
+
+app.post("/signup",(req,res)=>{
+    console.log("signup")
 })
