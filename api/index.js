f9ef12448f1cd108cfa75ff8255ef263296218f1
someswar177 <someswarasai.p21@iiits.in>
2024-03-23 00:33:28 +0530

    added database connection and updates index file

--- api/index.js
+++ api/index.js
@@ -1,1 +1,18 @@
-console.log("index.js")
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const dbConnect = require('./dbConnect');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const User = require('./models/userModel');
+const Bookmark = require('./models/bookmarkModel');
+
+dbConnect();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.listen(3000, () => {
+    console.log("server is listening at http://localhost:3000");
+})
