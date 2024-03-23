35b52af6a840fba4062afe6f9c3438aa223a2aab
someswar177 <someswarasai.p21@iiits.in>
2024-03-23 17:08:05 +0530

    added all backend routes

--- api/auth.js
+++ api/auth.js
@@ -3,11 +3,11 @@ const jwt = require('jsonwebtoken')
 module.exports = async(req,res,next)=>{
     try{
         const token = await req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(token,"SECRET_KEY")
+        const decodedToken = jwt.verify(token,"SECRET_KEY");
         const user = decodedToken;
         req.user = user;
         next();
-    }catch{
+    }catch(err){
         console.log(err)
         res.status(401).json({
             error:new Error("Invalid Request")
--- api/index.js
+++ api/index.js
@@ -16,10 +16,124 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
-app.listen(3000, () => {
-    console.log("server is listening at http://localhost:3000");
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`server is listening at http://localhost:${PORT}`);
 })
 
-app.post("/signup",(req,res)=>{
-    console.log("signup")
+app.post("/signup",async (req,res)=>{
+    const {email , password} = req.body;
+    try{
+        const hashedpassword = await bcrypt.hash(password,10);
+        const user = new User({
+            email:email,
+            password:hashedpassword
+        })
+        try{
+            await user.save()
+            res.status(200).send({
+                message:`User(${email}) created successfully`,
+                user
+            })
+        }catch{
+            res.status(500).send({
+                message:"Email already exists"
+            })
+        }
+    }catch{
+        res.status(500).send({
+            message:"password not hashed properly"
+        })
+    }
+})
+
+app.post('/login',async (req,res)=>{
+    const {email,password} = req.body;
+    const user = await User.findOne({email:email})
+    if(!user){
+        return(
+            res.status(500).send({
+                message:"email not found"
+            })
+        )
+    }
+    const passCheck = await bcrypt.compare(password,user.password);
+    if(!passCheck){
+        return(
+            res.status(500).send({
+                message:"Incorrect password"
+            }
+            )
+        )
+    }
+    const token = jwt.sign({
+        userId:user._id,
+        userEmail:user.email
+    },"SECRET_KEY",
+    {expiresIn:"24h"}
+    )
+    console.log(`token : ${token}`)
+    res.status(200).send({
+        message:"login successfully",
+        email:user.email,
+        token
+    })
+})
+
+app.get('/getuser',auth,async (req,res)=>{
+    console.log(req.user);
+    const user = await User.findOne({_id:req.user.userId})
+    res.status(500).send({
+        user,
+        message:"you are authorized to access"
+    })
+})
+
+app.get('/addbookmark',auth,async (req,res)=>{
+    try{
+        const newBookmark = new Bookmark({
+            id:req.body.id,
+            poster_path:req.body.poster_path,
+            backdrop_path:req.body.backdrop_path,
+            title:req.body.title
+        });
+        await newBookmark.save();
+        const user = await User.findByIdAndUpdate(
+            req.user.userId,
+            { $push:{bookmarks:newBookmark._id}},
+            {new:true}
+        );
+        console.log(user);
+        // res.json(user);
+    }catch(error){
+        console.error('Error adding bookmark:', error);
+        res.status(500).json({ message: "Already added to bookmark", error: 'Internal Server Error' });
+    }
+})
+
+app.get('/getbookmark',auth,async (req,res)=>{
+    console.log("this is getbookmark = ")
+    console.log(req.user)
+    try{
+        const user = await User.find({_id:req.user.userId});
+        if(!user){
+            res.status(404).json({ error: 'User not found' });
+            return;
+        }
+        console.log("user = ",user)
+        console.log("user bookmark = ",user[0].bookmarks);
+
+        const allBookmarks = []
+
+        for(let i=0;i< user[0].bookmarks.length;i++){
+            let eachBookmark = await Bookmark.findById(user[0].bookmarks[i]);
+            console.log("eachbookamrk = ",eachBookmark);
+            allBookmarks.push(eachBookmark);
+        }
+        console.log("allbookmarks = ",allBookmarks)
+        // res.json({allBookmarks})
+    }catch(err){
+        console.log(err)
+        res.status(500).json({error:"Internal server error"});
+    }
 })
